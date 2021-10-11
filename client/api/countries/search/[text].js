a4d44aaa9f112f5c964c8265a228fe474ea8bfe1
arcacndemo <arcacndemo@gmail.com>
2021-10-11 19:33:54 -0300

    text

--- client/api/countries/search/[text].js
+++ client/api/countries/search/[text].js
@@ -4,8 +4,8 @@ module.exports = async (req, res) => {
   try {
     const { text } = req.query;
     const country = await db.query(
-      "SELECT * FROM countries WHERE LOWER(name) LIKE LOWER(CONCAT('%', $1, '%')) ORDER by country_id",
-      [text.toString()]
+      "SELECT * FROM countries WHERE LOWER(name) LIKE LOWER(CONCAT('%', TEXT($1), '%')) ORDER by country_id",
+      [text]
     );
     res.json(country.rows);
   } catch (err) {
