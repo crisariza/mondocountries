da4cbe279ab010e7e8d0dfbc888afc89c9762040
arcacndemo <arcacndemo@gmail.com>
2021-10-11 19:48:07 -0300

    add

--- client/api/countries/order/[type]/[page].js
+++ client/api/countries/order/[type]/[page].js
@@ -3,14 +3,15 @@ let db = require("../../../db");
 module.exports = async (req, res) => {
   try {
     const { type, page } = req.query;
+    let countries = [];
 
     if (type === "alpdown") {
-      const countries = await db.query(
+      countries = await db.query(
         "SELECT * FROM countries WHERE country_id BETWEEN 250-($1*24)-($1-1) AND 250-(25*($1-1)) ORDER BY country_id DESC",
         [page]
       );
     } else if (type === "alpup") {
-      const countries = await db.query(
+      countries = await db.query(
         "SELECT * FROM countries WHERE country_id BETWEEN (25*$1)-24 AND 25*$1 ORDER BY country_id",
         [id]
       );
@@ -21,7 +22,7 @@ module.exports = async (req, res) => {
       type === "europe" ||
       type === "oceania"
     ) {
-      const countries = await db.query(
+      countries = await db.query(
         "SELECT * FROM countries WHERE LOWER(region) = LOWER($1) offset ((25*$2)-25) rows fetch next 25 rows only",
         [type, page]
       );
@@ -31,7 +32,7 @@ module.exports = async (req, res) => {
       type === "winter" ||
       type === "spring"
     ) {
-      const countries = await db.query(
+      countries = await db.query(
         "SELECT * FROM countries INNER JOIN activities ON cca3 = cca3  WHERE LOWER(cca3) = LOWER(cca3) AND LOWER(season) = LOWER($1) offset ((25*$2)-25) rows fetch next 25 rows only",
         [type, page]
       );
