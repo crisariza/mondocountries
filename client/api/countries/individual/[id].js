8627a4de5c1887a0cce9cb2f615f10315c2b68cc
arcacndemo <arcacndemo@gmail.com>
2021-10-11 17:08:11 -0300

    last

--- client/api/countries/individual/[id].js
+++ client/api/countries/individual/[id].js
@@ -2,7 +2,7 @@ let db = require("../../db");
 
 module.exports = async (req, res) => {
   try {
-    res.send(req, req.query);
+    console.log(req);
     const { id } = req.params;
     const country = await db.query(
       "SELECT * FROM countries WHERE LOWER(cca3) = LOWER($1)",
