f92b021ff1a25a684d245574edc25fb15582ac2b
arcacndemo <arcacndemo@gmail.com>
2021-10-11 19:11:07 -0300

    latest

--- client/api/countries/individual/[id].js
+++ client/api/countries/individual/[id].js
@@ -5,7 +5,7 @@ module.exports = async (req, res) => {
     const { id } = req.query;
     const country = await db.query(
       "SELECT * FROM countries WHERE LOWER(cca3) = LOWER($1)",
-      [2]
+      [id]
     );
     //const activity = await db.query(
     //"SELECT * FROM activities WHERE LOWER(cca3) LIKE LOWER(CONCAT('%', $1::letchar , '%')) ORDER by activity_id",
