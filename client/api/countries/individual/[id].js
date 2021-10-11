c993517ade23f1cd086b39d2409927a5743d5a06
arcacndemo <arcacndemo@gmail.com>
2021-10-11 17:26:18 -0300

    test

--- client/api/countries/individual/[id].js
+++ client/api/countries/individual/[id].js
@@ -3,14 +3,13 @@ let db = require("../../db");
 module.exports = async (req, res) => {
   try {
     const { id } = req.query;
-    console.log(id);
     const country = await db.query(
       "SELECT * FROM countries WHERE LOWER(cca3) = LOWER($1)",
-      [id]
+      [2]
     );
     const activity = await db.query(
       "SELECT * FROM activities WHERE LOWER(cca3) LIKE LOWER(CONCAT('%', $1::letchar , '%')) ORDER by activity_id",
-      [id]
+      [2]
     );
     res.json({ country: country.rows[0], activities: activity.rows });
   } catch (err) {
