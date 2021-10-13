ab9c88c7bc5d8f0d7ba8757123f2ebec0ae4fdb8
arcacndemo <arcacndemo@gmail.com>
2021-10-12 21:45:34 -0300

    finish

--- api/countries/order/[type]/[page].js
+++ api/countries/order/[type]/[page].js
@@ -7,7 +7,7 @@ module.exports = async (req, res) => {
     let countriesLength = 0;
     if (type === "alpup") {
       countries = await db.query(
-        "SELECT * FROM countries WHERE country_id BETWEEN (25*$1)-24 AND 25*$1 ORDER BY country_id",
+        "SELECT * FROM countries WHERE country_id BETWEEN (25*$1)-24 AND 25*$1 ORDER BY name",
         [page]
       );
       countriesLength = await db.query(
@@ -15,7 +15,7 @@ module.exports = async (req, res) => {
       );
     } else if (type === "alpdown") {
       countries = await db.query(
-        "SELECT * FROM countries WHERE country_id BETWEEN 250-($1*24)-($1-1) AND 250-(25*($1-1)) ORDER BY country_id DESC",
+        "SELECT * FROM countries WHERE country_id BETWEEN 250-($1*24)-($1-1) AND 250-(25*($1-1)) ORDER BY name DESC",
         [page]
       );
       countriesLength = await db.query(
